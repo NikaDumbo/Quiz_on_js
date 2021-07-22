22b0f3de210c427a9896dc026fc02bbdb99205e8
Nika Dumbadze <55450937+NikaDumbo@users.noreply.github.com>
2021-07-22 18:58:36 +0400

    Add files via upload

---
+++ scripts/myScript.js
@@ -0,0 +1,451 @@
+ Question = [ //Question and answer array
+    {
+      quest: "1. When you are in a community (at a party with fellow workers or fellow practitioners), you usually prefer?",
+      answer: ["A) participate in a general conversation", "B) talking with each separately",],
+      flag: [false, false],
+    },
+    {
+      quest: "2. You consider yourself more?",
+      answer: ["A) realistic", "B) prone to theorizing"],
+      flag: [false, false],
+    },
+    {
+      quest: "3. In your opinion, which one is worse?",
+      answer: ["A) 'living in a dream world'", "B) 'all goes according to the plan'"],  
+      flag: [false, false], 
+    },
+    {
+      quest: "4. You are more impressed by?",
+      answer: ["A) solid principles", "B) strong emotions"],
+      flag: [false, false],
+    },
+    {
+      quest: "5. You are more attracted?",
+      answer: ["A) persuasive", "B) sentimental "],
+      flag: [false, false],
+    },
+    {
+      quest: "6. If you have to do an unusual job, it's more convenient for you?",
+      answer: ["A) to plan it in advance", "B) to find out what to do, already in the course of work"],
+      flag: [false, false],
+    },
+   {
+      quest: "7. When you need to make a choice or make a decision, you make it?",
+      answer: ["A) mostly attentive and carefully", "B) often spontaneously"],
+      flag: [false, false],
+    },
+    {
+      quest: "8. At parties or social gatherings you usually?",
+      answer: ["A) stay up late", " B) leave early feeling tired"],
+      flag: [false, false],
+    },
+    {
+      quest: "9. You are more attracted to?",
+      answer: ["A) realists", " B) imaginative people",],
+      flag: [false, false],
+    },
+    {
+        quest: "10. You are more interested in?",
+        answer: ["A) what really exists", " B) unrealized opportunities"],
+        flag: [false, false],
+      },
+      {
+        quest: "11. Your conclusions about people based?",
+        answer: ["A) on rules more often than on circumstances", "B) on circumstances more often than on rules"],
+        flag: [false, false],
+      },
+      {
+        quest: "12. When assessing other people, you usually?",
+        answer: ["A) firm and objective", "B) condescending and subjective"],
+        flag: [false, false],
+      },
+      {
+        quest: "13. More often you act?",
+        answer: ["A) punctually", "B) slowly"],
+        flag: [false, false],
+      },
+      {
+        quest: "14. You prefer?",
+        answer: [" A) doing work in advance", "B) doing everything last moment"],
+        flag: [false, false],
+      },
+      {
+        quest: "15. Among your friends you?",
+        answer: [" A) you know everything about everyone", " B) the last to know what is happening"],
+        flag: [false, false],
+      },
+      {
+        quest: "16. When doing your usual work you prefer?",
+        answer: ["A) doing it the usual way", "B) inventing your own way"],
+        flag: [false, false],
+      },
+      {
+        quest: "17. When you read at your pleasure, you prefeer when a writer?",
+        answer: ["A) clearly explains what he means", "B) sets out thoughts in an unusual, original form."],
+        flag: [false, false],
+      },
+      {
+        quest: "18. What attracts you more?",
+        answer: ["A) consistency and reasoning", "B) the harmony of human relations"],
+        flag: [false, false],
+      },
+      {
+        quest: "19. It is easier for you to make judgments?",
+        answer: ["A) based on logic and on facts", " B) based on your values"],
+        flag: [false, false],
+      },
+      {
+        quest: "20. You are more attracted to situations?",
+        answer: ["A) in which there is an element of certainty", "B) which are full of unpredictability"],
+        flag: [false, false],
+      },
+      {
+        quest: "21. You can say that you are more?",
+        answer: ["A) serious and focused", "B) not taking life too seriously"],
+        flag: [false, false],
+      },
+      {
+        quest: "22. When talking on the phone?",
+        answer: ["A) you rarely think about: 'What to say?' in advance", "B) often think out in advance what you will say"],
+        flag: [false, false],
+      },
+      {
+        quest: "23. How do you think? Data?",
+        answer: [" A) speak for itself", " B) illustrates some patterns"],
+        flag: [false, false],
+      },
+      {
+        quest: "24. Dreamers?",
+        answer: ["A) annoy you", "B) attract and fascinate you"],
+        flag: [false, false],
+      },
+      {
+        quest: "25. More often you are?",
+        answer: ["A) calm and impartial", "B) cordial and sympathetic"],
+        flag: [false, false],
+      },
+      {
+        quest: "26. You think that it is worse to?",
+        answer: ["A) be unreasonable", "B) be malevolent"],
+        flag: [false, false],
+      },
+      {
+        quest: "27. In most cases it is better to?",
+        answer: ["A) try to control the course of events", "B) rely on the natural course of events."],
+        flag: [false, false],
+      },
+      {
+        quest: "28. You feel better when",
+        answer: ["A) you have already made a purchase", "B) you still have the opportunity to do it."],
+        flag: [false, false],
+      },
+      {
+        quest: "29. In the group of colleagues you tend to",
+        answer: ["A) initiate a conversation", "B) wait for someone to contact you"],
+        flag: [false, false],
+      },
+      {
+        quest: "30. Claims made on the basis of common sense",
+        answer: ["A) rarely raise doubts", "B) often raise doubts."],
+        flag: [false, false],
+      },
+      {
+        quest: "31. Would you like your friend to be a person",
+        answer: ["A) who stands firmly on the ground", "B) who always has new ideas."],
+        flag: [false, false],
+      },
+      {
+        quest: "32. When making decisions it is easier for you to follow",
+        answer: ["A) the rules and standards", "B) the wishes of interested parties"],
+        flag: [false, false],
+      },
+      {
+        quest: "33. You are rather a person",
+        answer: ["A) hard than soft", "B) soft than hard"],
+        flag: [false, false],
+      },
+      {
+        quest: "34. You are more attracted by the opportunity",
+        answer: ["A) to organize something", "B) creatively use the available opportunities"],
+        flag: [false, false],
+      },
+      {
+        quest: "35. In a situation you value",
+        answer: ["A) clarity", "B) the possibility of unpredictable turns."],
+        flag: [false, false],
+      },
+      {
+        quest: "36. Meeting new and unusual people.",
+        answer: ["A) stimulates and energizes you", "B) tires you"],
+        flag: [false, false],
+      },
+      {
+        quest: "37. In most cases you are",
+        answer: ["A) practical", "B) a dreamer "],
+        flag: [false, false],
+      },
+      {
+        quest: "38. You are more interested to know",
+        answer: ["A) wheather other people mayt be helpful to you", "B) other people's point of view"],
+        flag: [false, false],
+      },
+      {
+        quest: "39. You receive more satisfaction ",
+        answer: ["A) through a discussion of the issue", "B) through reaching agreement on the subject matter."],
+        flag: [false, false],
+      },
+      {
+        quest: "40. Your actions are more governed by",
+        answer: ["A) your head", "B) your heart"],
+        flag: [false, false],
+      },
+      {
+        quest: "41. When you know that at a certain time you will be engaged in a certain business,",
+        answer: ["A) you are glad that you can plan your time", "B) you don't like when you have to follow a certain plan"],
+        flag: [false, false],
+      },
+      {
+        quest: "42. Usually you",
+        answer: ["A) strive to achieve desired results", "B) are satisfied with what comes out"],
+        flag: [false, false],
+      },
+      {
+        quest: "43. Do you prefer to have",
+        answer: ["A) a wide circle of communication, consisting of many friends and acquaintances", "B) a narrow social circle consisting of several friends"],
+        flag: [false, false],
+      },
+      {
+        quest: "44. You are more guided by",
+        answer: ["A) facts", "B) regularities"],
+        flag: [false, false],
+      },
+      {
+        quest: "45. You are more interested or would be interested in",
+        answer: ["A) production and distribution of products", "B) research and design"],
+        flag: [false, false],
+      },
+      {
+        quest: "46. Do you consider it a compliment if you are called",
+        answer: ["A) a logical person", "B) the feeling person"],
+        flag: [false, false],
+      },
+      {
+        quest: "47. In yourself you value more",
+        answer: ["A) decisiveness", "B) devotion"],
+        flag: [false, false],
+      },
+      {
+        quest: "48. When you express your thoughts or judgments, you prefer them to be",
+        answer: ["A) completed", " B) preliminary with the possibility of improvement"],
+        flag: [false, false],
+      },
+      {
+        quest: "49. You feel better",
+        answer: ["A) after making an important decision", "B) before making an important decision."],
+        flag: [false, false],
+      },
+      {
+        quest: "50. When communicating with unfamiliar people, you",
+        answer: ["A) have long conversations", "B) hardly find topics for conversation"],
+        flag: [false, false],
+      },
+      {
+        quest: "51. You trust ",
+        answer: ["A) your experience", " B) your intuition"],
+        flag: [false, false],
+      },
+      {
+        quest: "52. Would you prefer to be considered as",
+        answer: ["A) a practical person", "B) an inventive person"],
+        flag: [false, false],
+      },
+      {
+        quest: "53. More praiseworthy is a man",
+        answer: [" A) who has 'clear mind'", "B) capable of strong feelings "],
+        flag: [false, false],
+      },
+
+      {
+        quest: "54. You are more likely to be",
+        answer: ["A) fair and impartial", "B) sympathetic and empathizing"],
+        flag: [false, false],
+      },
+      {
+        quest: "55. If you act according to a plan",
+        answer: [" A) you feel comfortable", "B) it constrains and limits you "],
+        flag: [false, false],
+      },
+      {
+        quest: "56. You do things better when you are",
+        answer: ["A) following a well-thought-out plan", "B) coping with surprises"],
+        flag: [false, false],
+      },
+      {
+        quest: " 57.Do you think that people close to you have your opinion on certain issues? ",
+        answer: ["A) know your opinion on certain issues", "B) will find out only when you tell them . "],
+        flag: [false, false],
+      },
+      {
+        quest: "58. In yourself you value",
+        answer: ["A) a strong sense of reality", "B) a vivid imagination "],
+        flag: [false, false],
+      },
+
+      {
+        quest: "59. If you were a teacher, you would prefer to teach ",
+        answer: ["A) practical subjects", " B) theoretical subjects "],
+        flag: [false, false],
+      },
+      {
+        quest: "60. What do you think is worse - to be ",
+        answer: ["A) too quick-tempered", "B) too objective"],
+        flag: [false, false],
+      },
+      {
+        quest: "61. You consider yourself as",
+        answer: ["A) sober-minded", "B) loyal and responsive "],
+        flag: [false, false],
+      },
+      {
+        quest: "62. You are more attracted to situations ",
+        answer: ["A) ordered and planned", "B) unordered and unplanned "],
+        flag: [false, false],
+      },
+      {
+        quest: "63. You are acting ",
+        answer: ["A) more often in accordance to the rules rather than the way you actually want", "B) more often the way you actually want rather than in accordance to the rules"],
+        flag: [false, false],
+      },
+      {
+        quest: "64. Usually you are",
+        answer: ["A) a sociable person", "B) calm and discreet"],
+        flag: [false, false],
+      },
+      {
+        quest: "65. When you are writing, you prefer",
+        answer: ["A) writing word for word", "B)  expressing  figuratively."],
+        flag: [false, false],
+      },
+      {
+        quest: "66. You find it harder",
+        answer: ["A) to put yourself in the shoes of other people", "B) to understand how to get the most benefit from other people."],
+        flag: [false, false],
+      },
+      {
+        quest: "67. Would you wish yourself",
+        answer: ["A) a greater mental clarity", "B) a greater compassion ability"],
+        flag: [false, false],
+      },
+      {
+        quest: "68. Would you prefer working with a manager who",
+        answer: ["A) is always fair", "B) is always benevolent."],
+        flag: [false, false],
+      },
+      {
+        quest: "69. Do you prefer",
+        answer: ["A) scheduled events", "B) spontaneous events"],
+        flag: [false, false],
+      },
+      {
+        quest: "70. You are more inclined",
+        answer: ["A) to think and plan", "B) to act according to circumstances"],
+        flag: [false, false],
+      },
+  ];
+
+class myQuiz{
+  constructor(Question){
+    this.myQuestionsArray = Question;
+    this.questionSwitcher();
+    this.questionDisplay();
+    this.getFirstQuestion();
+
+  }
+
+  questionSwitcher(){
+    let body = document.getElementsByTagName("body")[0];
+    let container = document.createElement("div");
+    container.setAttribute("class", "questionContainer");
+    body.appendChild(container);
+    this.myQuestionsArray.forEach((arr, index) => {
+      let questionButton = document.createElement("button");
+      let questionNumber = document.createTextNode("Question "+(index + 1));
+      questionButton.setAttribute("id", "question"+(index+1));
+      questionButton.setAttribute("class", "questionChoice");
+      questionButton.setAttribute("onclick", "getQuestion("+index+")");
+      questionButton.appendChild(questionNumber);
+      container.appendChild(questionButton);
+    });  
+  }
+
+  questionDisplay(){
+    let body = document.getElementsByTagName("body")[0];
+    let display = document.createElement("div");
+    display.setAttribute("class", "displayContainer");
+    body.appendChild(display);
+    let question = document.createElement("div");
+    question.setAttribute("id", "questionText");
+    question.setAttribute("class", "questionText");
+    display.appendChild(question);
+    let functionalButtons = document.createElement("div");
+   // question.setAttribute("id", "buttonContainer");
+    functionalButtons.setAttribute("class", "buttonContainer");
+    let finishButton = document.createElement("button");
+    let finishButtonText = document.createTextNode("End Quiz");
+    finishButton.setAttribute("onclick", "swal('Quiz finished successfully!', '', 'success')");
+    finishButton.appendChild(finishButtonText);
+    functionalButtons.appendChild(finishButton);
+    let restartButton = document.createElement("button");
+    let restartButtonText = document.createTextNode("Restart");
+    restartButton.setAttribute("onclick", "location.reload()");
+    restartButton.appendChild(restartButtonText);
+    functionalButtons.appendChild(restartButton);
+    display.appendChild(functionalButtons);
+
+  }
+
+  getFirstQuestion(){
+    document.getElementById("questionText").innerHTML = Question[0].quest + "<br><br>"
+  + "<input type='radio' id="+"answerA"+0+" name="+"answer"+0+">"+Question[0].answer[0] +" "
+  + "<input type='radio' id="+"answerB"+0+" name="+"answer"+0+">"+Question[0].answer[1];
+
+  let btn = document.getElementById("question"+(0+1));
+  let radioA =  document.getElementById("answerA"+0);
+  let radioB =  document.getElementById("answerB"+0);
+  Question.forEach((arr, index) => {
+    if(Question[index].flag[0] == false && Question[index].flag[1] == false)
+      document.getElementById("question"+(index+1)).style.backgroundColor="transparent";
+    if(Question[index].flag[0] == true || Question[index].flag[1] == true)
+      document.getElementById("question"+(index+1)).style.backgroundColor="green";
+  });
+  btn.style.backgroundColor="#F26651";
+  radioA.addEventListener('change', () => {btn.style.backgroundColor="green"; Question[0].flag[0] = true; Question[0].flag[1] = false;});
+  radioB.addEventListener('change', () => {btn.style.backgroundColor="green"; Question[0].flag[1] = true; Question[0].flag[0] = false;});
+  }
+
+}
+
+function getQuestion(questionNumber){
+  document.getElementById("questionText").innerHTML = Question[questionNumber].quest + "<br><br>"
+  + "<input type='radio' id="+"answerA"+questionNumber+" name="+"answer"+questionNumber+">"+Question[questionNumber].answer[0] +" "
+  + "<input type='radio' id="+"answerB"+questionNumber+" name="+"answer"+questionNumber+">"+Question[questionNumber].answer[1];
+  setButtonColor(questionNumber);
+}
+
+function setButtonColor(questionNumber){
+  let btn = document.getElementById("question"+(questionNumber+1));
+  let radioA =  document.getElementById("answerA"+questionNumber);
+  let radioB =  document.getElementById("answerB"+questionNumber);
+  Question.forEach((arr, index) => {
+    if(Question[index].flag[0] == false && Question[index].flag[1] == false)
+      document.getElementById("question"+(index+1)).style.backgroundColor="transparent";
+    if(Question[index].flag[0] == true || Question[index].flag[1] == true)
+      document.getElementById("question"+(index+1)).style.backgroundColor="green";
+  });
+  btn.style.backgroundColor="#F26651";
+  radioA.addEventListener('change', () => {btn.style.backgroundColor="green"; Question[questionNumber].flag[0] = true; Question[questionNumber].flag[1] = false;});
+  radioB.addEventListener('change', () => {btn.style.backgroundColor="green"; Question[questionNumber].flag[1] = true; Question[questionNumber].flag[0] = false;});
+}
+
+
+let q1 = new myQuiz(Question);
